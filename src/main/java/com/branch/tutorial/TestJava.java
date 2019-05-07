71cf8ea656a4855359487f694314aa0823b9adfb
Krishnaarunangsu <arunangsutech@gmail.com>
2019-05-07 09:06:56 +0530

    first commit

---
+++ src/main/java/com/branch/tutorial/TestJava.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.branch.tutorial;
+
+/**
+ * @author arunangsu.sahu
+ *
+ */
+public class TestJava {
+
+	/**
+	 * 
+	 */
+	public TestJava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
